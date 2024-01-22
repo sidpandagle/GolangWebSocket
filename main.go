3985717e3ba9552914f092a8e559ac2f42bed49a
spandagle <siddhant.pandagle1998@gmail.com>
2024-01-22 14:33:22 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Client struct {
+	conn *websocket.Conn
+}
+
+var clients = make(map[*Client]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Sender  *Client
+	Content string
+}
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	client := &Client{conn: conn}
+	clients[client] = true
+
+	defer func() {
+		delete(clients, client)
+		client.conn.Close()
+	}()
+
+	for {
+		var message Message
+		err := conn.ReadJSON(&message)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		broadcast <- message
+	}
+}
+
+func handleMessages() {
+	for {
+		message := <-broadcast
+		for client := range clients {
+			err := client.conn.WriteJSON(message)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	go handleMessages()
+	http.HandleFunc("/", handleConnection)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+}
